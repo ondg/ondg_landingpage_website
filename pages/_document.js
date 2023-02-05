9e8f6a79b211b9ffc11a9b97e1a338c9da4b3549
klaycer <kwamzyasare@gmail.com>
2023-02-05 12:35:51 +0000

    wip: document changes

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
       </Head>
       <body>
@@ -21,19 +21,19 @@ function Document() {
         <Script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
         ></Script>
 
         <Script
             src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
             integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
         ></Script>
 
         <Script
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
             integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
         ></Script>
       </body>
     </Html>
